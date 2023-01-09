f0638d8f355bb2ea525e46d48230d898797cbf50
shubkeshtpss <67048285+shubkeshtpss@users.noreply.github.com>
2023-01-09 08:28:08 +0530

    Magnific popup

---
+++ magnific.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet">    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.3.2/css/lightgallery.css" />
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.3.2/js/lightgallery.js"></script>
+    <link href="jquery.bsPhotoGallery.css" rel="stylesheet">
+    <script src="jquery.bsPhotoGallery.js"></script>	
+	
+    </script>
+    <script>
+      $(document).ready(function(){
+        $('ul.first').bsPhotoGallery({
+          "classes" : "col-xl-3 col-lg-2 col-md-4 col-sm-4",
+          "hasModal" : true,
+          "shortText" : true  
+        });
+      });
+    </script>
+  </head>
+
+  <body>
+    <div class="container">
+        
+
+        <ul class="row first">
+            <li>
+                <img alt="Rocking the night away"  src="http://demo.michaelsoriano.com/images/photodune-174908-rocking-the-night-away-xs.jpg">
+                <h2>title</h2>
+                <p>Consectetur adipiscing elit44444  </p>
+            </li>
+            <li>
+                <img alt="Yellow sign"  src="http://demo.michaelsoriano.com/images/photodune-287182-blah-blah-blah-yellow-road-sign-xs.jpg">
+                   
+                <p>Lorem ipsum dolor sit amet, labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+            </li>
+            
+
+  </ul>
+
+
+
+    </div> <!-- /container -->
+
+
+
+  </body>
+
+
+</html>
